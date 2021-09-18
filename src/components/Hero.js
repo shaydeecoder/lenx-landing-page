627a4632ec50e7659004aa9b4f0cb08899af67cc
shaydeecoder <shaydeecoder@gmail.com>
2021-09-18 21:10:17 +0100

    Updated hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,13 +4,13 @@ const Hero = () => {
   return (
     <section className="hero container--fluid">
       <div className="hero__inner container">
-        <div className="inner__description">
+        <div className="inner__description inner__description--1">
           <h1 className="inner__description--heading">
             Make every sale with pleasure.
           </h1>
         </div>
 
-        <div className="inner__description">
+        <div className="inner__description inner__description--2">
           <p className="inner__description--body">
             We work with manufacturing brands and have created a web application
             for you to get more sales and easily communicate with your buyers.
