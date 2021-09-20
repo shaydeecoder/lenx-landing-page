7974c0b8691fec837d60764b84d91d16fad34c30
shaydeecoder <shaydeecoder@gmail.com>
2021-09-20 21:39:27 +0100

    Added icons to features section and fixed input auto-fill bug

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,5 +1,11 @@
 import FeaturesCard from "./FeaturesCard";
 
+// Importing icons
+import icons8_buying from "../img/icons/icons8_buying.png";
+import icons8_chat_bubble from "../img/icons/icons8_chat_bubble.png";
+import icons8_expensive from "../img/icons/icons8_expensive.png";
+import icons8_learn_more from "../img/icons/icons8_learn_more.png";
+
 const Features = () => {
   return (
     <section className="features container-fluid section">
@@ -10,7 +16,7 @@ const Features = () => {
 
         <div className="features__cards-wrap">
           <FeaturesCard
-            icon="icon"
+            icon={icons8_buying}
             heading="Better Buyer Experience"
             body="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum
               repellendus obcaecati consequuntur tempore, vero quis ad saepe a!
@@ -18,7 +24,7 @@ const Features = () => {
           />
 
           <FeaturesCard
-            icon="icon"
+            icon={icons8_chat_bubble}
             heading="Easy Communication"
             body="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum
               repellendus obcaecati consequuntur tempore, vero quis ad saepe a!
@@ -26,7 +32,7 @@ const Features = () => {
           />
 
           <FeaturesCard
-            icon="icon"
+            icon={icons8_learn_more}
             heading="Carry Your Customers Along"
             body="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum
               repellendus obcaecati consequuntur tempore, vero quis ad saepe a!
@@ -34,7 +40,7 @@ const Features = () => {
           />
 
           <FeaturesCard
-            icon="icon"
+            icon={icons8_expensive}
             heading="Get Paid Directly"
             body="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum
               repellendus obcaecati consequuntur tempore, vero quis ad saepe a!
